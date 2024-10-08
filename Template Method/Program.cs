fe4d92d3f978fb82a96b9a1e4e873b09ae73dd1e
yasiia <malykyasya@gmail.com>
2024-10-08 11:20:24 +0300

    Add project files.

---
+++ Template Method/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Security;
+
+namespace Template_Method
+{
+    public abstract class TemplateMetodActivity
+    {
+        public void SandwichAlgoritm()
+        {
+            CutBread();
+            AddMainIngredient();
+            AddVegetables();
+            AddSauce();
+            Serve();
+        }
+        protected void CutBread()
+        {
+            Console.WriteLine("Sliced ​​bread");
+        }
+        protected abstract void AddMainIngredient();
+        protected abstract void AddVegetables();
+        protected abstract void AddSauce();
+        protected void Serve()
+        {
+            Console.WriteLine("Your sandwich is ready!");
+        }
+    }
+
+    public class MeatSandwich : TemplateMetodActivity
+    {
+        protected override void AddMainIngredient()
+        {
+            Console.WriteLine("Add meat and cheasse");
+        }
+        protected override void AddVegetables()
+        {
+            Console.WriteLine("Add salat and tomatos");
+        }
+        protected override void AddSauce()
+        {
+            Console.WriteLine("Add mayonnaise and mustard");
+        }
+    }
+
+    public class VegetablesSandwich : TemplateMetodActivity
+    {
+        protected override void AddMainIngredient()
+        {
+            Console.WriteLine("Add cheese");
+        }
+
+        protected override void AddVegetables()
+        {
+            Console.WriteLine("Add salad and tomato");
+        }
+
+        protected override void AddSauce()
+        {
+            Console.WriteLine("Add olive oil");
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args)
+        {
+            TemplateMetodActivity meatSandwich = new MeatSandwich();
+            Console.WriteLine("Start cooking");
+            meatSandwich.SandwichAlgoritm();
+
+            TemplateMetodActivity vegetablesSandwich = new VegetablesSandwich();
+            Console.WriteLine("Start cooking");
+            vegetablesSandwich.SandwichAlgoritm();
+
+        }
+    }
+    
+}
